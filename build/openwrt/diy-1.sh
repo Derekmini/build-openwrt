2d4cb4079700ce2fe3632e9065d480e364ec531b
Derek <mguangwe@gmail.com>
2021-02-18 20:19:13 +0800

    update openwrt config

--- build/openwrt/diy-1.sh
+++ build/openwrt/diy-1.sh
@@ -9,5 +9,28 @@
 #
 # 请不要直接套用我其他机型的扩展或者插件，每个源码有些文件路径都多多少少不一样，直接套用会出错
 #
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# Add ssr-plus
+# fw876/helloworld
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+# Add luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+
+# Add Jerrykuku's packages(vssr/jd-daily/argon theme)
+rm -rf package/lean/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/jerrykuku/lua-maxminddb
+git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus package/jerrykuku/luci-app-jd-dailybonus
+git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr package/jerrykuku/luci-app-vssr
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/jerrykuku/luci-app-argon-config
+
+# Add luci-app-adguardhome
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+
+# Add luci-app-dockerman
+rm -rf package/lean/luci-app-docker
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+# git clone --depth=1 https://github.com/KFERMercer/luci-app-dockerman package/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker package/lisaac/luci-lib-docker
+
